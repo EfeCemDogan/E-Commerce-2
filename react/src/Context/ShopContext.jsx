91f69064933320ebda706a903bbea0b7024e08b1
Efe Cem Dogan <125884274+EfeCemDogan@users.noreply.github.com>
2023-12-27 22:02:04 +0300

    ChangeToReact

--- react/src/Context/ShopContext.jsx
+++ react/src/Context/ShopContext.jsx
@@ -1,17 +1,15 @@
-import React, { createContext, useState } from "react";
-import all_product from "../Components/Assets/all_product";
+import React, {createContext, useState} from "react";
+import all_product from '../Components/Assets/all_product'
 
-  
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () => {
     let cart = {};
-    for (let product of all_product) {
-        cart[product.id] = 0;
+    for (let index = 0; index < all_product.length+1; index++) {
+        cart[index] = 0;
     }
-    return cart;
+    return cart
 }
-
 
 const ShopContextProvider = (props) => {
 
@@ -21,7 +19,7 @@ const ShopContextProvider = (props) => {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
         console.log(cartItems);
     }
-    
+
     const removeFromCart = (itemId) => {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
     }
